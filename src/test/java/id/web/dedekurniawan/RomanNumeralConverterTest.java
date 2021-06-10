af05fe865f8fe1eb28d80143ef752f27551f33d4
dede.kurniawan <blue_shadow_bird@yahoo.co.id>
2021-06-10 15:55:56 +0700

    Add Test Case and Refactor Package

--- src/test/java/id/web/dedekurniawan/RomanNumeralConverterTest.java
+++ src/test/java/id/web/dedekurniawan/RomanNumeralConverterTest.java
@@ -1,13 +1,22 @@
 package id.web.dedekurniawan;
 
+import id.web.dedekurniawan.exception.InformationException;
+import id.web.dedekurniawan.exception.RomanConvertException;
+import id.web.dedekurniawan.utility.RomanNumeralConverter;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class RomanNumeralConverterTest {
     @Test
-    public void test(){
+    public void testConvertSuccess() throws RomanConvertException {
         Assert.assertEquals(1903, RomanNumeralConverter.convert("MCMIII"));
         Assert.assertEquals(1944, RomanNumeralConverter.convert("MCMXLIV"));
         Assert.assertEquals(2006, RomanNumeralConverter.convert("MMVI"));
+    }
+
+    @Test
+    public void testConvertRepetationMoreThan3(){
+        Assert.assertThrows(RomanConvertException.class, () -> RomanNumeralConverter.convert("MMMM"));
+        Assert.assertThrows(RomanConvertException.class, () -> RomanNumeralConverter.convert("MLLLL"));
     }
 }
