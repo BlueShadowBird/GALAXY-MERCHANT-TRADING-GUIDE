ca4be5ed7d1a7acbec53e0ca3c6c24e431eb01b7
dede.kurniawan <blue_shadow_bird@yahoo.co.id>
2021-06-10 16:02:08 +0700

    Add Use Case

--- src/test/java/id/web/dedekurniawan/EntityTest.java
+++ src/test/java/id/web/dedekurniawan/EntityTest.java
@@ -19,7 +19,7 @@ public class EntityTest {
         aliases.addInformation("tegj is L");
     }
     @Test
-    public void testSuccess() throws InformationException {
+    public void testAddEntity() throws InformationException {
         Entity entity = new Entity(aliases);
         entity.addInformation("glob glob Silver is 34 Credits");
         entity.addInformation("glob prok Gold is 57800 Credits");
@@ -29,5 +29,11 @@ public class EntityTest {
         Assert.assertEquals(17.0, entity.getValue("Silver"));
         Assert.assertEquals(14450.0, entity.getValue("Gold"));
         Assert.assertEquals(195.5, entity.getValue("Iron"));
+    }
+
+    @Test
+    public void testAddEntityFail() throws InformationException {
+        Entity entity = new Entity(aliases);
+        Assert.assertThrows(InformationException.class, () -> entity.addInformation("glob glo Silver is 34 Credits"));
     }
 }
