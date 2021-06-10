a02a91d0cf0a1139edd7293c13a666157ff94af9
dede.kurniawan <blue_shadow_bird@yahoo.co.id>
2021-06-10 15:56:58 +0700

    Add handling for invalid queries

--- src/main/java/id/web/dedekurniawan/Main.java
+++ src/main/java/id/web/dedekurniawan/Main.java
@@ -1,14 +1,19 @@
 package id.web.dedekurniawan;
 
+import id.web.dedekurniawan.exception.InformationException;
+import id.web.dedekurniawan.exception.RomanConvertException;
+import id.web.dedekurniawan.model.Aliases;
+import id.web.dedekurniawan.model.Entity;
+import id.web.dedekurniawan.utility.RomanNumeralConverter;
+
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, InformationException {
+    public static void main(String[] args) throws IOException, InformationException, RomanConvertException {
         Aliases aliases = new Aliases();
 
         List<String> lines = new ArrayList<>();
@@ -35,19 +40,37 @@ public class Main {
         }while(!line.endsWith("?"));
 
         while(true){
-            if(line.startsWith("how much is")){
-                line = line.replace("how much is", "").replace("?", "").trim();
-                System.out.println(line + " is " + RomanNumeralConverter.convert(convertRomanAlias(aliases, line)));
-            }else if(line.startsWith("how many Credits is")){
-                line = line.replace("how many Credits is", "").replace("?", "").trim();
-                String[] splitedLine = line.split(" ");
-                String entityName = splitedLine[splitedLine.length-1];
-                splitedLine = Arrays.copyOf(splitedLine, splitedLine.length-1);
+            try {
+                if(line.startsWith("how much is")){
+                    line = line.replace("how much is", "").replace("?", "").trim();
+                    System.out.println(line + " is " + RomanNumeralConverter.convert(convertRomanAlias(aliases, line)));
+                }else if(line.startsWith("how many Credits is")){
+                    line = line.replace("how many Credits is", "").replace("?", "").trim();
+                    String[] splitedLine = line.split(" ");
+                    String entityName = splitedLine[splitedLine.length-1];
+                    splitedLine = Arrays.copyOf(splitedLine, splitedLine.length-1);
 
-                double credits = (RomanNumeralConverter.convert(convertRomanAliasSplited(aliases, splitedLine))*(double)entity.getValue(entityName));
-                System.out.println(line + " is " + credits + " Credits");
-            }else{
-                System.out.println("I have no idea what you are talking about");
+                    Double price = (Double)entity.getValue(entityName);
+                    if(price == null)
+                        throw new InformationException("Unknown Entity", entityName);
+
+                    double credits = (RomanNumeralConverter.convert(convertRomanAliasSplited(aliases, splitedLine))*price);
+                    System.out.println(line + " is " + format(credits) + " Credits");
+                }else{
+                    System.out.println("I have no idea what you are talking about");
+                }
+            }catch (RomanConvertException e){
+                if("Max Symbol Repetation exceeded".equals(e.getMessage()))
+                    System.out.println("Requested number is in invalid format");
+                else
+                    throw e;
+            }catch (InformationException e){
+                if("Unknown Alias".equals(e.getMessage()))
+                    System.out.println("Unknown alias for " + e.getErrorArgument());
+                else if("Unknown Entity".equals(e.getMessage()))
+                    System.out.println("Unknown Entity for " + e.getErrorArgument());
+                else
+                    throw e;
             }
 
             if(lines.isEmpty())
@@ -58,11 +81,11 @@ public class Main {
     }
 
     private static String convertRomanAliasSplited(Aliases aliases, String[] expressionSplited) throws InformationException {
-        StringBuffer romanSymbol = new StringBuffer();
+        StringBuilder romanSymbol = new StringBuilder();
         for (String s : expressionSplited) {
             String symbol = (String) aliases.getValue(s);
             if (symbol == null)
-                throw new InformationException("Unknown Alias: " + s);
+                throw new InformationException("Unknown Alias", s);
             romanSymbol.append(symbol);
         }
         return romanSymbol.toString();
@@ -70,5 +93,13 @@ public class Main {
 
     private static String convertRomanAlias(Aliases aliases, String expression) throws InformationException {
         return convertRomanAliasSplited(aliases, expression.split(" "));
+    }
+
+    private static String format(double d)
+    {
+        if(d == (long) d)
+            return String.format("%d",(long)d);
+        else
+            return String.format("%s",d);
     }
 }
--- src/main/java/id/web/dedekurniawan/exception/InformationException.java
+++ src/main/java/id/web/dedekurniawan/exception/InformationException.java
@@ -1,8 +1,14 @@
-package id.web.dedekurniawan;
+package id.web.dedekurniawan.exception;
 
 public class InformationException extends Exception {
+    String errorArgument;
     public InformationException() { super(); }
     public InformationException(String message) { super(message); }
+    public InformationException(String message, String errorArgument) { super(message); this.errorArgument=errorArgument; }
     public InformationException(String message, Throwable cause) { super(message, cause); }
     public InformationException(Throwable cause) { super(cause); }
+
+    public String getErrorArgument() {
+        return errorArgument;
+    }
 }
