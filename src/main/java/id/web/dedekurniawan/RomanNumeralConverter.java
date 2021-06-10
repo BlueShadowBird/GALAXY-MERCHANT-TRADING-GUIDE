40b12edae87789b43dc53cb392a9e48cc2bf5614
dede.kurniawan <blue_shadow_bird@yahoo.co.id>
2021-06-10 11:26:52 +0700

    RomanNumeralConverter logic and test

--- src/main/java/id/web/dedekurniawan/RomanNumeralConverter.java
+++ src/main/java/id/web/dedekurniawan/RomanNumeralConverter.java
@@ -1,16 +1,48 @@
 package id.web.dedekurniawan;
 
-public class RomanNumeralConverter {
-    private static int I = 1;
-    private static int V = 5;
-    private static int X = 10;
-    private static int L = 50;
-    private static int C = 100;
-    private static int D = 500;
-    private static int M = 1000;
+import java.util.HashMap;
+import java.util.Map;
 
-    public static int convert(String RomanNumber){
+public class RomanNumeralConverter {
+    private static final Map<Character, Integer> romanMap;
+    static {
+        romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+    }
+
+    public static int convert(String romanNumber){
+        System.out.println(romanNumber);
         int result = 0;
+        byte[] bytes = romanNumber.getBytes();
+        int bytesLength = bytes.length;
+        for(int i=0;i<bytesLength;i++){
+            if(i<bytesLength-1){
+                if('I'==bytes[i] && ('V'==bytes[i+1] || 'X'==bytes[i]+1)){
+                    result += romanMap.get((char) bytes[i + 1]) - romanMap.get((char) bytes[i]);
+                    i++;
+                }else if('X'==bytes[i] && ('L'==bytes[i+1] || 'C'==bytes[i+1])){
+
+                        result += romanMap.get((char) bytes[i + 1]) - romanMap.get((char) bytes[i]);
+                        i++;
+
+                }else if('C'==bytes[i] && ('D'==bytes[i+1] || 'M'==bytes[i+1])){
+
+                        result += romanMap.get((char) bytes[i + 1]) - romanMap.get((char) bytes[i]);
+                        i++;
+
+                }else{
+                    result += romanMap.get((char) bytes[i]);
+                }
+            }else{
+                result += romanMap.get((char) bytes[i]);
+            }
+        }
 
         return result;
     }
--- src/test/java/id/web/dedekurniawan/RomanNumeralConverterTest.java
+++ src/test/java/id/web/dedekurniawan/RomanNumeralConverterTest.java
@@ -1,4 +1,13 @@
 package id.web.dedekurniawan;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class RomanNumeralConverterTest {
+    @Test
+    public void test(){
+        Assert.assertEquals(1903, RomanNumeralConverter.convert("MCMIII"));
+        Assert.assertEquals(1944, RomanNumeralConverter.convert("MCMXLIV"));
+        Assert.assertEquals(2006, RomanNumeralConverter.convert("MMVI"));
+    }
 }
