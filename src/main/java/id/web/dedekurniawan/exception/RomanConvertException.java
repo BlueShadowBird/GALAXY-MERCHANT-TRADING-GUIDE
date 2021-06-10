84f422298e5dc0691e7f5b5237a0757fd076787b
dede.kurniawan <blue_shadow_bird@yahoo.co.id>
2021-06-10 15:55:24 +0700

    Add Exception

---
+++ src/main/java/id/web/dedekurniawan/exception/RomanConvertException.java
@@ -0,0 +1,8 @@
+package id.web.dedekurniawan.exception;
+
+public class RomanConvertException extends Exception{
+    public RomanConvertException() { super(); }
+    public RomanConvertException(String message) { super(message); }
+    public RomanConvertException(String message, Throwable cause) { super(message, cause); }
+    public RomanConvertException(Throwable cause) { super(cause); }
+}
--- src/main/java/id/web/dedekurniawan/utility/RomanNumeralConverter.java
+++ src/main/java/id/web/dedekurniawan/utility/RomanNumeralConverter.java
@@ -1,8 +1,17 @@
-package id.web.dedekurniawan;
+package id.web.dedekurniawan.utility;
+
+import id.web.dedekurniawan.exception.RomanConvertException;
 
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Class utility for converting Roman Number to Decimal. Static Class that not need to create instances
+ *
+ * @author  Dede Kurniawan
+ * @version 1.0
+ * @since   2021-06-10
+ */
 public class RomanNumeralConverter {
     private static final Map<Character, Integer> romanMap;
     static {
@@ -16,11 +25,29 @@ public class RomanNumeralConverter {
         romanMap.put('M', 1000);
     }
 
-    public static int convert(String romanNumber){
+    /**
+     * Convert Roman Number to Decimal.
+     * @param: String of roman Number
+     * @return: the decimal value for Roman Number
+     *
+     * @author  Dede Kurniawan
+     * @version 1.0
+     * @since   2021-06-10
+     */
+    public static int convert(String romanNumber) throws RomanConvertException {
         int result = 0;
         byte[] bytes = romanNumber.getBytes();
         int bytesLength = bytes.length;
+        int repetition = 1;
+        byte lastChar = 0;
         for(int i=0;i<bytesLength;i++){
+            if(lastChar==bytes[i]){
+                if(++repetition>3)//max repeated 3, no more repetition allowed
+                    throw new RomanConvertException("Max Symbol Repetation exceeded");
+            }else{
+                lastChar=bytes[i];
+                repetition = 1;
+            }
             if(i<bytesLength-1){
                 if('I'==bytes[i] && ('V'==bytes[i+1] || 'X'==bytes[i]+1)){
                     result += romanMap.get((char) bytes[i + 1]) - romanMap.get((char) bytes[i]);
