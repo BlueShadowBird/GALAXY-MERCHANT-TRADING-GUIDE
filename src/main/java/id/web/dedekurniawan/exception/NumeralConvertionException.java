f5ae3e8a9b7bddfcce8b358936f9846c0362f8a8
dede.kurniawan <blue_shadow_bird@yahoo.co.id>
2021-06-25 08:28:11 +0700

    make less coupled with interface, so if need using other numeral symbol so just need implement the interface assign it

---
+++ src/main/java/id/web/dedekurniawan/exception/NumeralConvertionException.java
@@ -0,0 +1,8 @@
+package id.web.dedekurniawan.exception;
+
+public class NumeralConvertionException extends Exception{
+    public NumeralConvertionException() { super(); }
+    public NumeralConvertionException(String message) { super(message); }
+    public NumeralConvertionException(String message, Throwable cause) { super(message, cause); }
+    public NumeralConvertionException(Throwable cause) { super(cause); }
+}
--- src/main/java/id/web/dedekurniawan/exception/RomanConvertException.java
+++
@@ -1,8 +0,0 @@
-package id.web.dedekurniawan.exception;
-
-public class RomanConvertException extends Exception{
-    public RomanConvertException() { super(); }
-    public RomanConvertException(String message) { super(message); }
-    public RomanConvertException(String message, Throwable cause) { super(message, cause); }
-    public RomanConvertException(Throwable cause) { super(cause); }
-}
